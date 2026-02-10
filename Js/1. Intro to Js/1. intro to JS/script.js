fd45bde3cc05e4682acd4e96879a961ec08397b0
Gagandeep Singh <gagan240806@gmail.com>
2026-02-10 15:09:33 +0530

    Creating a stable structure

---
+++ Js/1. Intro to Js/1. intro to JS/script.js
@@ -0,0 +1,31 @@
+let name = "Gagandeep Singh"; 
+        let age = 19;
+        let Is_Eligible_For_Vote = true;
+        console.log("My name is " , name);
+        // console.log("My age is " + age);
+        console.log("My age is " , age);
+        console.log("Is eligibel for vote:-" , Is_Eligible_For_Vote);
+            
+
+        // let a = 10;
+        // let b = 91;
+        // const sum = a+b;
+        // console.log(a+b);
+        // console.log(sum);
+        // n lines of code
+        // console.log(a+b-1); Does same operation twice that's why we store it in a variable and use it later.
+        // console.log(sum - 1);
+
+        // const varName = "Gagandeep Singh";
+        // varName = "Gagan"; // This will give an error because we cannot change the value of a constant variable.
+        // console.log(varName);
+
+        // let varName = "Gagandeep Singh";
+        // varName = "Gagan"; // This will not give an error because we can change the value of a let variable.
+        // console.log(varName);
+
+        //Main diffrence between let and const is that we can change the value of a let variable but we cannot change the value of a const variable. 
+
+        // Diff btw let and var.
+        // var = Global scope
+        // let = local scope 
