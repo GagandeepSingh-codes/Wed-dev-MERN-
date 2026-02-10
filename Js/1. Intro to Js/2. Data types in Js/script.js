9b0a63e53ebf024a83639dac7abb7fc0f44b7583
Gagandeep Singh <gagan240806@gmail.com>
2026-02-10 23:27:15 +0530

    Adding file of data types in Js

---
+++ Js/1. Intro to Js/2. Data types in Js/script.js
@@ -0,0 +1,41 @@
+// let a = 123;
+// a = "Gagan"
+// a = "69"
+// a = true
+// console.log("The value of a is :-" ,a);
+// console.log("The data type of a is :-" ,typeof(a));  
+
+// let b = 2;
+// let c =  1;
+// let sum = null;
+// let sum = 0;
+// sum = a+b ;
+// console.log("The sum of a and b is :-" , sum );
+// console.log("The data type of sum is :-" ,typeof(sum));  
+
+// let v1 = 12;
+// console.log(v1+1);
+
+// let v2 = "12";
+// console.log(v2+1);
+
+// let result;
+// let abc = 123;
+// let result = String(abc); /* Can be converted into strings*/
+
+// console.log("The type of abc is :-", typeof(abc));
+// console.log("The type of result is :-", typeof(result));
+
+// let name = "gagan";
+// let name = "123";
+// name = Number(name); /* Can be converted into number*/
+
+// console.log("The value for name is :-", name); /* NaN :- Not a number for name = "gagan"*/
+// console.log("The type of  name  is :-", typeof(name));
+// console.log("The type of  name  is :-", typeof(name));
+
+let name = -23; /* Other than 0 all value are refered to as "true"*/
+name = Boolean(name); /* Can be converted into number*/
+
+console.log("The value for name is :-", name); /* NaN :- Not a number for name = "gagan"*/
+console.log("The type of  name  is :-", typeof(name));
