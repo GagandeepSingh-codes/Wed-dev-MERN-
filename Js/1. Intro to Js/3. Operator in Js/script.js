9ed6aaeba6034ca3bdce537c9378c6afdbceb5ab
Gagandeep Singh <gagan240806@gmail.com>
2026-02-12 00:39:29 +0530

    Adding file related to Operators in Js

---
+++ Js/1. Intro to Js/3. Operator in Js/script.js
@@ -0,0 +1,90 @@
+// // Assignment operator
+// const a = 3;
+// const b = 6;
+// const sum = a+b;
+
+// console.log("The sum of a and b is :-",sum);
+
+// // Increment operator (++)
+// let abc = 12;
+// abc++;
+// console.log("The value after Increment",abc);
+ 
+// // Decrement operator (--)
+// let xyz = 13;
+// xyz--;
+// console.log("The value after Decrement",xyz);
+
+// // Post Increment
+// console.log("The values after Post-increment")
+// let asd = 10;
+
+// console.log("asd :-",asd);
+// console.log("asd++ :-" , asd++);
+// console.log("asd :-",asd);
+
+// // Pre Increment
+// console.log("The values after Pre-increment")
+// let asb = 10;
+
+// console.log("asb :-",asb);
+// console.log("++asb :-" , ++asb);
+// console.log("asb :-",asb);
+
+// // Post Decrement 
+// console.log("The values after post-decrement")
+// let asp = 10;
+
+// console.log("asp :-",asp);
+// console.log("asp++ :-" , asp++);
+// console.log("asp :-",asp);
+
+// // Pre Decrement 
+// console.log("The values after Pre-decrement")
+// let ase = 10;
+
+// console.log("ase :-",ase);
+// console.log("++ase :-" , ++ase);
+// console.log("ase :-",ase);
+
+// // Exponentiation operator
+// let a = 2;
+// let b = 3;
+// const result = a**b;
+// console.log("The expoantiation of a and b :-",result);
+
+// // Short hand operator
+// let a = 3;
+// let b = 4;
+// a += b ; /* a = a+b; */ 
+
+// console.log("Using short hand operator :-",a);
+
+// // Comparsion operator
+// let a =3;
+// let b ="3";
+
+// console.log("the value after using comprasion operator (Normal equals form) :-" , a==b); /*Normal equals form */
+// console.log("the value after using comprasion operator (Strict equals form) :-" , a===b); /*Strict equals form */
+
+// Logical operator
+// Logical AND (&&)
+// Logical OR (||)
+// Logical NOT (!)
+
+
+// // Logical AND (&&)
+// let a = true;
+// let b = false;
+// const result = a && b;
+// console.log(result);
+
+// // Logical OR (||)
+// let a = true;
+// let b = false;
+// const result = a || b;
+// console.log(result);
+
+// // Logical NOT (!)
+// let a = true;
+// console.log(!a);
