8f9ca4876eee4e506a0da4b50583e5aa6b5c27f5
Gagandeep Singh <gagan240806@gmail.com>
2026-02-12 15:43:16 +0530

    Adding file for f-else condition with examples

---
+++ Js/1. Intro to Js/4. If-else in Js/script.js
@@ -0,0 +1,55 @@
+// Making a voter eligiblilty system 
+// // If condition
+// const candidateAge = 23;
+// if (candidateAge>=18){
+//     console.log("Congrat!!!\nYou are eligible for vote :)");  
+// } 
+
+// // If-else condition
+// const candidateAge = 13;
+// if (candidateAge>=18){
+//     console.log("Congrat!!!\nYou are eligible for vote :)");  
+// } else{
+//     console.log("Sorry!!!\nYou are not eligible for vote :(");   
+// }
+
+// // If-else condition or Else-if ladder
+// const candidateAge = 13;
+// if (candidateAge>=60){
+//     console.log("Senior citizen!!!\nYou can vote both online or offline mode (prefered online) :)");  
+// }else if(candidateAge>=18){
+//     console.log("Younger citizen!!!\nYou are eligible for vote , have to be physically present.(offline mode) :)");
+// }
+// else{
+//     console.log("Youngest citizen!!!\nYou are not eligible for vote :(");   
+// }
+
+// // Making a grade assigning system
+// console.log("CALCULATING YOUR GRADES!!!");
+
+// const marks = 99;
+
+// if(marks>=95){
+//     console.log("Excellent grade :- A++\nKeep it up my boy!!!");
+// }
+// else if(marks>=90){
+//     console.log("Great grade :- A+\nGreat job!!!");
+// }
+// else if(marks>=85){
+//     console.log("Good grade :- B++\nGood job!!!");
+// }
+// else if(marks>=80){
+//     console.log("Better grade :- B+\nBetter job!!!");
+// }
+// else if(marks>=70){
+//     console.log("Average grade :- C\nAverage job!!!");
+// }
+// else{
+//     console.log("Below Average grade :- D\nYou have potential, do better!!!");
+// }
+// console.log("There is always hope of improvement!!!");
+
+// Ternary operator
+// condition ? true : false
+const cAge = 15;
+cAge>= 18 ? console.log("Congrats!!!\nYou are eligible to vote :)") : console.log("Sorry!!!\nYou are not yet eligible to vote officially :( ");
