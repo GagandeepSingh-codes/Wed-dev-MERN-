974c30f6f57474f2892c6f65c040f5b39bd9051e
Gagandeep Singh <gagan240806@gmail.com>
2026-02-17 01:35:21 +0530

    pushing all the updates

---
+++ Js/1. Intro to Js/8. Functions in Js/script.js
@@ -0,0 +1,17 @@
+// // To add to numbers 
+// let a = 12;
+// let b = 24;
+// let sum = a+b;
+// console.log(sum);
+
+// But rather than writing the same code again and again to add two numbers we can create a function.
+
+// Function :- A function is a block of code that performs a specific task. We can call the function whenever we want to perform that task.
+
+function summation(a, b){
+    let sum = a+b ;
+    console.log("The sum of a and b is :- ",sum);
+}
+summation(12,24);
+summation(100,200);
+
