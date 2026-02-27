83e729a37d0c6c1801d2f50967a9c692a4650f64
Gagandeep Singh <gagan240806@gmail.com>
2026-02-27 14:28:25 +0530

    Pushing update for call back hell file

---
+++ Js/10. Call back hell in Js/script.js
@@ -0,0 +1,23 @@
+console.log("Bro,");
+console.log("Consistency");
+console.log("is");
+console.log("the");
+console.log("key");
+console.log("to");
+setTimeout(function(){
+    console.log("This is exceuted after 5sec, using setTimeout");
+}, 5000);
+console.log("success");
+
+setTimeout(function(){
+    console.log("Bro!");
+}, 1000);
+setTimeout(function(){
+    console.log("BE CONSISTENT IN YOUR WORK AND HAVE PATIENCE ");
+}, 2000);
+setTimeout(function(){
+    console.log("SUCCESS WILL DEFINITELY COME TO YOU!");
+}, 4000);
+
+
+// This is called callback hell, when we have to do multiple asynchronous operations one after another, we end up with nested callbacks which can be hard to read and maintain. To avoid this, we can use Promises or async/await in JavaScript.
